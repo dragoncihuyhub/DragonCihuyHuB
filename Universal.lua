7e262a562c433d5bde4f6553168614b8986c731e
dragoncihuyhub <dragoncihuy55@gmail.com>
2026-05-27 07:23:11 +0700

    Create Universal.lua

---
+++ Universal.lua
@@ -0,0 +1,27 @@
+-- Universal game
+-- Yang support script yang game apa pun
+
+-- Map Babft 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ilhammaul110412-ui/Dragoncihuyscript/main/DRAGONCIHUYY_Babft.lua"))() 
+
+-- Map Dive down 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dragoncihuyhub/DragonCihuyHuB/refs/heads/main/Divedown.lua"))()
+
+-- Map Draw Wheel For Brainrots 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dragoncihuyhub/DragonCihuyHuB/refs/heads/main/Draw_Wheel_For_Brainrots.lua"))()
+
+-- Map Roller For Brainrots 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dragoncihuyhub/DragonCihuyHuB/refs/heads/main/Roller_For_Brainrots.lua"))()
+
+-- Map Climb And Jump Tower 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dragoncihuyhub/DragonCihuyHuB/refs/heads/main/Climb_And_Jump_Tower.lua"))()
+
+-- Map Climb and Jump Tower 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dragoncihuyhub/DragonCihuyHuB/refs/heads/main/Climb_And_Jump_Tower.lua
+
+-- Map Petak Umpet 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dragoncihuyhub/DragonCihuyHuB/refs/heads/main/Petak_Umpet.lua"))()
+
+-- Map Prison Life
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dragoncihuyhub/DragonCihuyHuB/refs/heads/main/Prison_Life.lua"))()
+    
